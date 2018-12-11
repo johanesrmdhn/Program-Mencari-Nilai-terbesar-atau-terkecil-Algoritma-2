0241008653ca01939a41c19978ca1e56e4323bfe
Johanes Ramadhan <45659505+johanesrmdhn@users.noreply.github.com>
2018-12-12 00:56:11 +0700

    Add files via upload

---
+++ Nilai terbesar atau terkecil.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	
+	int a,b;
+	cout<<"masukkan nilai a : ";cin>>a;
+	cout<<"masukkan nilai b : ";cin>>b;
+	if(a<b)
+	{
+		cout<<"b terbesar\n";
+	}
+	else if(a>b)
+	{
+		cout<<"a terbesar\n";
+	}else
+	cout<<"error";
+	return 0;
+	
+}
